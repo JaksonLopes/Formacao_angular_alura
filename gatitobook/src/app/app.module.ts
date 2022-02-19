09c0edfe97f31c13691b5818da9bba798121e20c
jackson <jacksonlopes948@gmail.com>
2022-02-19 15:32:16 -0300

    finalizando autenticação de login ,e utilizando token

--- gatitobook/src/app/app.module.ts
+++ gatitobook/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+import { RodapeComponent } from './componentes/rodape/rodape.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
